e1948b7d2391f01cf8fa59c35ebf3ea3606d832d
ahadsarwar-ops <wahab.waqar@nearearthadventures.com>
2026-09-11 04:43:28 -0700

    fix(api): implement safe multi-path decorator resolution for class-validator in Vercel serverless environment

--- apps/api/src/common/class-validator.ts
+++ apps/api/src/common/class-validator.ts
@@ -1,13 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cv = require('class-validator')
+/* eslint-disable @typescript-eslint/no-var-requires */
+let decorators: any = {}
+try {
+  decorators = require('class-validator/cjs/decorator/decorators')
+} catch {
+  try {
+    decorators = require('class-validator')
+  } catch {}
+}
 
-export const IsOptional: any = cv.IsOptional
-export const IsString: any = cv.IsString
-export const IsEnum: any = cv.IsEnum
-export const IsInt: any = cv.IsInt
-export const IsNumber: any = cv.IsNumber
-export const Min: any = cv.Min
-export const Max: any = cv.Max
-export const IsArray: any = cv.IsArray
-export const ValidateNested: any = cv.ValidateNested
-export const IsUUID: any = cv.IsUUID
+const getDecorator = (name: string): any => {
+  if (typeof decorators[name] === 'function') return decorators[name]
+  if (decorators.decorators && typeof decorators.decorators[name] === 'function') {
+    return decorators.decorators[name]
+  }
+  return () => () => {}
+}
+
+export const IsOptional: any = getDecorator('IsOptional')
+export const IsString: any = getDecorator('IsString')
+export const IsEnum: any = getDecorator('IsEnum')
+export const IsInt: any = getDecorator('IsInt')
+export const IsNumber: any = getDecorator('IsNumber')
+export const Min: any = getDecorator('Min')
+export const Max: any = getDecorator('Max')
+export const IsArray: any = getDecorator('IsArray')
+export const ValidateNested: any = getDecorator('ValidateNested')
+export const IsUUID: any = getDecorator('IsUUID')
