6f18e6301a5796cd4033ff37e9047bccbe5ee1a7
ahadsarwar-ops <wahab.waqar@nearearthadventures.com>
2026-09-11 04:28:59 -0700

    fix(api): replace internal class-validator cjs subpath require with main package require to fix Vercel deployment module error

--- apps/api/src/common/class-validator.ts
+++ apps/api/src/common/class-validator.ts
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const decorators = require('class-validator/cjs/decorator/decorators')
+const cv = require('class-validator')
 
-export const IsOptional: any = decorators.IsOptional
-export const IsString: any = decorators.IsString
-export const IsEnum: any = decorators.IsEnum
-export const IsInt: any = decorators.IsInt
-export const IsNumber: any = decorators.IsNumber
-export const Min: any = decorators.Min
-export const Max: any = decorators.Max
-export const IsArray: any = decorators.IsArray
-export const ValidateNested: any = decorators.ValidateNested
-export const IsUUID: any = decorators.IsUUID
+export const IsOptional: any = cv.IsOptional
+export const IsString: any = cv.IsString
+export const IsEnum: any = cv.IsEnum
+export const IsInt: any = cv.IsInt
+export const IsNumber: any = cv.IsNumber
+export const Min: any = cv.Min
+export const Max: any = cv.Max
+export const IsArray: any = cv.IsArray
+export const ValidateNested: any = cv.ValidateNested
+export const IsUUID: any = cv.IsUUID
